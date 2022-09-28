c4c237779a3820bc95193d08404caebd29fecda4
Meo <minhlongitf@gmail.com>
2022-09-28 20:18:08 +0700

    update

--- main.js
+++ main.js
@@ -1,45 +1,29 @@
-var result = [];
+var result = [],
+	input = document.getElementById("content-input"),
+	output = document.getElementById("content-target");
 
-document.getElementById('input-file')
-    .addEventListener('change', getFile)
-
-function getFile(event) {
-    const input = event.target
-    if ('files' in input && input.files.length > 0) {
-        placeFileContent(document.getElementById('content-target'), input.files[0])
-    }
-}
-
-function placeFileContent(target, file) {
-    readFileContent(file).then(content => {
-        convertProxy(content)
-        result.pop();
-        let format = result.toString().replaceAll(",", "\n");
-        target.value = format
-    }).catch(error => console.log(error))
-}
-
-function readFileContent(file) {
-    const reader = new FileReader()
-    return new Promise((resolve, reject) => {
-        reader.onload = event => resolve(event.target.result)
-        reader.onerror = error => reject(error)
-        reader.readAsText(file)
-    })
+function convert() {
+	convertProxy(input.value.replaceAll(/\s/g, ",").replaceAll(",,", ","));
+	let format = result.toString().replaceAll(",", "\n");
+	output.value = format
 }
 
 function convertProxy(proxy) {
-    const array = proxy.replaceAll(/\s/g,'').split(",").map(String);
-
-    array.map(element => {
-        let el = element.split(":")
-        const px = `${el[2]}:${el[3]}@${el[0]}:${el[2]}`;
-        result.push(px);
-    })
+	const array = proxy.replaceAll(/\s/g, "").split(",").map(String);
+	array.map(element => {
+		let el = element.split(":");
+		console.log(el);
+		const px = `${el[2]}:${el[3]}@${el[0]}:${el[1]}`;
+		result.push(px)
+	})
 }
 
 function copy() {
-    let textarea = document.getElementById("content-target");
-    textarea.select();
-    document.execCommand("copy");
+	document.getElementById("content-target").select(), document.execCommand("copy")
 }
+
+document.getElementById("clear").addEventListener("click", function() {
+	input.value = "";
+	output.value = "";
+});
+
